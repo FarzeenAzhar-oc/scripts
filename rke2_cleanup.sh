09558bd4290aea7f9b7676d5ee85ad140838764c
FarzeenAzhar-oc <119649459+FarzeenAzhar-oc@users.noreply.github.com>
2023-03-30 11:18:35 +0530

    Add files via upload

---
+++ rke2_cleanup.sh
@@ -0,0 +1,20 @@
+curl https://raw.githubusercontent.com/rancher/system-agent/main/system-agent-uninstall.sh | sudo sh
+sudo rke2-uninstall.sh
+sudo rm -rf /etc/ceph \
+       /etc/cni \
+       /etc/kubernetes \
+       /etc/rancher \
+       /opt/cni \
+       /opt/rke \
+       /run/secrets/kubernetes.io \
+       /run/calico \
+       /run/flannel \
+       /var/lib/calico \
+       /var/lib/etcd \
+       /var/lib/cni \
+       /var/lib/kubelet \
+       /var/lib/rancher\
+       /var/log/containers \
+       /var/log/kube-audit \
+       /var/log/pods \
+       /var/run/calico
